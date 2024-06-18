8dd415daa03e048b1641873ccf0229abffe6b9cc
Karthi <keyan182718@gmail.com>
2024-06-18 07:17:59 +0530

    contact form changes

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,15 @@ import AboutUs from "./components/AboutUs/aboutUs";
 import Skills from "./components/skills/skills";
 import Experience from "./components/Experience/Experience";
 // import Project from "./components/Project/Project";
-// import ContactForm from "./components/ContactForm/ContactForm";
+import ContactForm from "./components/ContactForm/ContactForm";
 import ProjectList from "./components/ProjectList/ProjectList";
 import Footer from "./components/Footer/Footer";
 import './App.css';
+import { createContext, useContext, useState } from "react";
 function App() {
+  let [contactToggle,setContactToggle]=useState(false);
   return <div className="overall-container">
-    <div className="icon-div"><i className="fa fa-address-book"></i></div>
+    <div className="icon-div" onClick={()=>setContactToggle(!contactToggle)}><i className="fa fa-address-book"></i></div>
     <WelcomeBanner />
     <Navbar />
     <HomeBanner />
@@ -19,7 +21,7 @@ function App() {
     <Skills />
     <Experience />
     <ProjectList />
-    {/* <ContactForm />  */}
+    {contactToggle && <ContactForm toggleFunction={setContactToggle}/>} 
     {/* <Project />
     */}
     <Footer />
--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -1,25 +1,32 @@
+import { useContext } from 'react';
 import './ContactForm.css';
 // import contactImage from "../../assets/contact.jpg"
-const ContactForm = () => {
+const ContactForm = ({toggleFunction}) => {
     return (
         <div className='contact-form-container'>
             <div className="contact-us-header d-flex justify-content-between align-items-center">
-                <h6>Reach me!</h6>
-                <i className='fa fa-close'></i>
+                <h6 className='header-title'>Reach out now!</h6>
+                <i className='fa fa-close close-icon' onClick={()=>toggleFunction(false)}></i>
             </div>
-            <div>
-                <form action="">
+            <form action="">
+            <div className='contact-form-content'>
+
                 <div className="form-group">
-    <label >Email address</label>
-    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
-    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+    <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter your name"/>
+    
+  </div>
+                <div className="form-group">
+    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter your email"/>
   </div>
   <div className="form-group">
-    <label >Password</label>
-    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
+    <textarea className="form-control" id="exampleFormControlTextarea1" placeholder="Enter your Message" rows={5}></textarea>
   </div>
-                </form>
+
+            <div className="contact-form-button">
+                <button className='btn primary-button text-white'>Connect with Me</button>
             </div>
+            </div>
+            </form>
         </div>
     )
 }
