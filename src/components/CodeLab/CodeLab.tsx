db71da0cd2cf9a84a2bb7af8ddc6142ae98021a9
Karthi <keyan182718@gmail.com>
2024-11-30 16:27:30 +0530

    remove unused values

--- src/components/CodeLab/CodeLab.tsx
+++ src/components/CodeLab/CodeLab.tsx
@@ -1,6 +1,6 @@
 import { motion } from 'framer-motion';
 import './CodeLab.css';
-import { useQuery,gql } from '@apollo/client';
+import { useQuery } from '@apollo/client';
 import { getCodeLabsData } from '../../gql/Queries';
 import { useEffect } from 'react';
 interface codeLabProps {
